4488461f708ffc47a8bafa549a06738b7d6371d7
NoPosh <murin8@mail.ru>
2025-07-15 19:22:32 +0300

    Character Rotate
    Более адекватный поворот персонажа

--- Assets/_project/Scripts/PlayerController.cs
+++ Assets/_project/Scripts/PlayerController.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 2f;
+
     public float turnSpeed = 100f;
+    public float maxTurnSpeed = 1f;
+    [Tooltip("–ассто€ние при максимальной скорости поворота в пиксел€х")]
+    public float maxDistanceForTurn = 200f;
+
     public bool isMoving = false;
 
     private int leftTouchFingerId = -1;
     private float previousTochX;
+    private float startTochX;
+    
 
     private void Update()
     {
@@ -21,26 +28,39 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
-    } 
-    
-    void HandleRotation()           //ѕока зажато, нужно свер€ть начальной точкой нажати€, и пока не отпущено не переставать поворачивать, если палец дальше половины экрана, тоже не перестовать
+    }
+
+    //ѕока зажато, нужно свер€ть начальной точкой нажати€,
+    //и пока не отпущено не переставать поворачивать,
+    //≈сли палец дальше половины экрана, тоже не перестовать
+    void HandleRotation()           
     {
 #if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0) && Input.mousePosition.x < Screen.width / 2)
         {
-            previousTochX = Input.mousePosition.x;
+            startTochX = Input.mousePosition.x;
         }
         else if (Input.GetMouseButton(0))
         {
-            if (Input.mousePosition.x < Screen.width / 2)
+            if (startTochX != 0)
             {
-                float deltaX = Input.mousePosition.x - previousTochX;
-                transform.Rotate(Vector3.up, deltaX * turnSpeed * Time.deltaTime / 10f);
-                previousTochX = Input.mousePosition.x;
+                float deltaX = Input.mousePosition.x - startTochX;
+                float deltaRotate = Mathf.Sign(deltaX);
+                float distance = Mathf.Abs(deltaX);
+
+                float t = Mathf.Clamp01(distance / maxDistanceForTurn);
+                // ѕолучаем плавную скорость поворота
+                float smoothRotate = Mathf.Lerp(0, maxTurnSpeed, t);
+
+                transform.Rotate(Vector3.up, deltaRotate * smoothRotate * turnSpeed * Time.deltaTime);
             }
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            startTochX = 0;
+        }
 
-#else
+#else   //ѕеределать управление как на компе
         foreach (Touch touch in Input.touches)
         {
             if (touch.phase == TouchPhase.Began && touch.position.x < Screen.width / 2)
