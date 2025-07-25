8b7a300409d7d1be5acc59a77a3e1571642b11c2
NoPosh <murin8@mail.ru>
2025-07-25 15:44:39 +0300

    Screenshot
    Можно делать скриншоты
    Предметы подбираются

---
+++ Assets/_project/Scripts/Screen/TransparentSceenshot.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.IO;
+
+//В дальнейшем можно сделать, чтобы была кнопка в инспекторе или как Tool
+public class TransparentSceenshot : MonoBehaviour
+{
+    public Camera screenshotCamera;
+    public RenderTexture renderTexture;
+
+    [ContextMenu("Take Screenshot")]
+    public void TakeScreenshot()
+    {
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+
+        screenshotCamera.Render();
+
+        Texture2D image = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
+        image.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        image.Apply();
+
+        byte[] bytes = image.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/Stone.png", bytes);
+
+        RenderTexture.active = currentRT;
+
+        Debug.Log("Saved screenshot with alpha at " + Application.dataPath + "/Stone.png");
+    }
+}
